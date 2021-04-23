5b36a20d367a3e88a95d1bc8780118fb2ea3c661
Joel Gratcyk <joel@gratcyk.com>
2021-04-23 08:24:39 -0500

    Update copypasteinto_functions.php

--- copypasteinto_functions.php
+++ copypasteinto_functions.php
@@ -1,5 +1,5 @@
 /**
- * Remove Topbar Admin Menu Link to Theme Customizer
+ * Remove Theme Customizer Link in WP Topbar Menu
  */
 add_action( 'admin_bar_menu', 'remove_some_nodes_from_admin_top_bar_menu', 999 );
 function remove_some_nodes_from_admin_top_bar_menu( $wp_admin_bar ) {
